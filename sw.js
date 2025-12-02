27ecb3a2688da130384d19314f5e48a20644b102
supachai2001774 <koaexol2025@gmail.com>
2025-12-02 11:58:53 +0700

    Initial commit

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'miner-app-v1';
+const ASSETS = [
+  './indexApp.html',
+  './style.css',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;600&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
